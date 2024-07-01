8877621805c05a4afd0ce5b199ef27a93227c082
Yago Moreira <yagomoreira@gmail.com>
2024-07-01 19:25:21 -0300

    add: POO com Java: Fundamentos da POO - Classes

---
+++ poo-com-java/src/main/java/com/bootcamp/fundamentosPoo/classes/Student.java
@@ -0,0 +1,20 @@
+package com.bootcamp.fundamentosPoo.classes;
+
+import java.awt.Color;
+
+public class Student {
+  String name;
+  int age;
+//  Color color;
+//  Sex sex;
+
+//  void eating(Foo food){
+//    //NOSSO CÓDIGO AQUI
+//  }
+//  void drinking(Eat eat){
+//    //NOSSO CÓDIGO AQUI
+//  }
+//  void running(){
+//    //NOSSO CÓDIGO AQUI
+//  }
+}
